c94aa6a7dc95a37a01bf317ad6eb35fe35b17ca0
Rod Conan <rconan@gmto.org>
2022-06-14 00:06:05 +0000

    ADD ANOTHER OPTICAL MODEL

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use crseo::{Diffractive, FromBuilder, ShackHartmann};
+use crseo::{Atmosphere, Diffractive, FromBuilder, ShackHartmann};
 use dos_actors::{
     clients::{
         arrow_client::{Arrow, Get},
@@ -12,36 +12,66 @@ use dos_actors::{
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    let mut timer: Initiator<_> = Timer::new(5).into();
-    let n_px_frame = 64;
+    let mut timer: Initiator<_> = Timer::new(3).into();
+    let n_px_frame = 512;
     let mut optical_model: Actor<_> = (
         OpticalModel::builder()
-            .options(vec![OpticalModelOptions::ShackHartmann {
-                options: ShackHartmannOptions::Diffractive(
-                    ShackHartmann::<Diffractive>::builder().detector(
-                        n_px_frame,
-                        None,
-                        Some(4),
-                        None,
+            .options(vec![
+                OpticalModelOptions::ShackHartmann {
+                    options: ShackHartmannOptions::Diffractive(
+                        ShackHartmann::<Diffractive>::builder(),
                     ),
-                ),
-                flux_threshold: 0f64,
-            }])
+                    flux_threshold: 0f64,
+                },
+                OpticalModelOptions::Atmosphere {
+                    builder: Atmosphere::builder(),
+                    time_step: 5e-3,
+                },
+            ])
             .build()?,
         "On-axis GMT",
+    )
+        .into();
+    let mut adaptive_optics: Actor<_> = (
+        OpticalModel::builder()
+            .options(vec![
+                OpticalModelOptions::ShackHartmann {
+                    options: ShackHartmannOptions::Diffractive(
+                        ShackHartmann::<Diffractive>::builder(),
+                    ),
+                    flux_threshold: 0f64,
+                },
+                OpticalModelOptions::Atmosphere {
+                    builder: Atmosphere::builder(),
+                    time_step: 5e-3,
+                },
+            ])
+            .build()?,
+        "Adaptive Optics",
     )
         .into();
     let logging = Arrow::builder(5).build().into_arcx();
     let mut logs: Terminator<_> = Actor::new(logging.clone());
 
+    let ao_logging = Arrow::builder(5).build().into_arcx();
+    let mut ao_logs: Terminator<_> = Actor::new(ao_logging.clone());
+
     timer
         .add_output()
+        .multiplex(2)
         .build::<Tick>()
-        .into_input(&mut optical_model);
+        .into_input(&mut optical_model)
+        .into_input(&mut adaptive_optics)
+        .confirm()?;
     optical_model
         .add_output()
         .build::<WfeRms>()
         .log(&mut logs)
+        .await;
+    adaptive_optics
+        .add_output()
+        .build::<WfeRms>()
+        .log(&mut ao_logs)
         .await;
     optical_model
         .add_output()
@@ -57,7 +87,9 @@ async fn main() -> anyhow::Result<()> {
     Model::new(vec![
         Box::new(timer),
         Box::new(optical_model),
+        Box::new(adaptive_optics),
         Box::new(logs),
+        Box::new(ao_logs),
     ])
     .name("dos101")
     .flowchart()
@@ -68,6 +100,7 @@ async fn main() -> anyhow::Result<()> {
 
     let data: Vec<Vec<f64>> = (*logging.lock().await).get("WfeRms")?;
     println!("{:?}", data);
-
+    let data: Vec<Vec<f64>> = (*ao_logging.lock().await).get("WfeRms")?;
+    println!("{:?}", data);
     Ok(())
 }
